c9b576cea664c5d304b2db6de1674df1e31aef2b
Ben Marshall <bmarsha1@umbc.edu>
2021-12-02 14:41:22 -0500

    Initial Commit

---
+++ UMBC App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  UMBC App
+//
+//  Created by Ben Marshall on 12/2/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ UMBC App/UMBC_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  UMBC_AppApp.swift
+//  UMBC App
+//
+//  Created by Ben Marshall on 12/2/21.
+//
+
+import SwiftUI
+
+@main
+struct UMBC_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
