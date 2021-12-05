430aff1ae1269184a410799de29740b2160036ec
Ben Marshall <bmarsha1@umbc.edu>
2021-12-05 17:23:03 -0500

    Use MKMapView

--- UMBC App/Map/MapView.swift
+++ UMBC App/Map/MapView.swift
@@ -8,36 +8,42 @@
 import SwiftUI
 import MapKit
 
-struct MapView: View {
-    var building: Building
-    @State private var region = MKCoordinateRegion()
-    var body: some View {
-        Map(
-            coordinateRegion: $region,
-            annotationItems: [building]
-        ) {item in
-            MapAnnotation (
-                coordinate: building.locationCoordinate,
-                content: {
-                    Image(systemName: "pin.circle.fill").foregroundColor(.red)
-                    Text(building.name).font(.system(size: 10))
-                }
-            )
-        }
-            .onAppear {
-                setRegion(building.locationCoordinate)
-            }
+struct MapView: UIViewRepresentable {
+    var coordinate: CLLocationCoordinate2D
+    func makeUIView(context: Context) -> MKMapView {
+        MKMapView()
     }
-    private func setRegion(_ coordinate: CLLocationCoordinate2D){
-        region = MKCoordinateRegion(
+    func updateUIView(_ uiView: MKMapView, context: Context) {
+        let region = MKCoordinateRegion(
             center: coordinate,
             span: MKCoordinateSpan(latitudeDelta: 0.003, longitudeDelta: 0.003)
         )
+        uiView.setRegion(region, animated: true)
+        uiView.showsBuildings = true
+        uiView.pointOfInterestFilter = MKPointOfInterestFilter.excludingAll
+        uiView.showsUserLocation = true
+        for building in buildings{
+            uiView.addAnnotation(BuildingAnnotation(building: building))
+        }
+    }
+}
+
+class BuildingAnnotation: NSObject, MKAnnotation {
+    let title: String?
+    let subtitle: String?
+    let coordinate: CLLocationCoordinate2D
+    let building : Building
+    
+    init(building: Building){
+        self.title = building.name
+        self.subtitle = ""
+        self.coordinate = building.locationCoordinate
+        self.building = building
     }
 }
 
 struct MapView_Previews: PreviewProvider {
     static var previews: some View {
-        MapView(building: buildings[0])
+        MapView(coordinate: CLLocationCoordinate2D(latitude: 39.25296, longitude: -76.71252))
     }
 }
